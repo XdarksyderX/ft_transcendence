53094afaf4aff121670c6b364e8e425df0921d5c
jariza-o <juanarizaordonez@gmail.com>
2024-10-07 18:48:42 +0200

    PostgreSQL connection Working

--- dockers/mywebsite/tools/utils.sh
+++ dockers/mywebsite/tools/utils.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
-
 # Colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m'
+
+
+# Configuración adicional de PostgreSQL
+echo "Configurando pg_hba.conf"
+echo 'local all postgres md5' >> "$PGDATA/pg_hba.conf"
+
+# Actualizaciones adicionales de configuración, por ejemplo:
+echo "listen_addresses='*'" >> "$PGDATA/postgresql.conf"
+
+
+# Config PostgreSQL
+service postgresql start
+su - postgres
+# psql -c "CREATE USER admin WITH PASSWORD '$PostgreSQL_User' SUPERUSER;"
+# psql -c "CREATE DATABASE $PostgreSQL_DBName OWNER $PostgreSQL_User;"
+# psql -c "GRANT ALL PRIVILEGES ON DATABASE $PostgreSQL_DBName TO $PostgreSQL_User;"
+su - postgres -c "psql -c \"CREATE USER $PostgreSQL_User WITH PASSWORD '$POSTGRES_PASSWORD' SUPERUSER;\""
+su - postgres -c "psql -c \"CREATE DATABASE $POSTGRES_DB OWNER $PostgreSQL_User;\""
+su - postgres -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $PostgreSQL_User;\""
+
+service postgresql restart
+
+
+
+# Add password of PostgreSQL in Django connector
+echo "localhost:5432:$POSTGRES_DB:$PostgreSQL_User:$POSTGRES_PASSWORD" > /conf/.pgpass
+# NOSE SI HAY QUE METER PERMISOS
 
 # Make Migrations
 python3 manage.py makemigrations
@@ -12,6 +38,7 @@ python3 manage.py migrate
 
 # Create the superuser
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', '${DJANGO_ADMIN_PASSWORD}')"
+
 
 # Run the development server
 exec "$@"
--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -89,10 +89,24 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# New Sistem for connect PostgreSQL
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': os.getenv('POSTGRES_DB', 'chat'),
+        'USER': os.getenv('PostgreSQL_User', 'postgres'),
+        # 'USER': 'postgres',
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', '1Qwerty70$'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
