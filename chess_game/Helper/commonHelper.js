2148a45ff1ed0f56477a127197febe19757219c0
Marina Alba Rodriguez Estebanez <marina.rodriguez1357@gmail.com>
2024-11-01 15:43:35 +0100

    bishop move wip

--- chess_game/Helper/commonHelper.js
+++ chess_game/Helper/commonHelper.js
@@ -34,9 +34,49 @@ function checkSquareCaptureId(array) {
     return (returnArray);
 }
 
-//function to give highlight ids for bishop
+//function to give highlight ids for bishop. The movement is diagonal
 function giveBishopHighlightIds(id) {
-    console.log(keySquareMapper[id]);
+    //will give top left ids
+    function topLeft(id) {
+        let alpha = id[0];
+        let num = Number(id[1]);
+        let returnArr = [];
+
+        while (alpha == "a" || num == 8) {
+            num = Number(id[1]) + 1;
+            alpha = String.fromCharCode(id[0].charCodeAt(0) - 1);
+            returnArr.push(`${alpha}${num}`);
+        }
+    
+        //return `${String.fromCharCode(id[0].charCodeAt(0) - 1)}${num}`;
+        return returnArr;
+    }
+    //will give bottom left ids
+    function bottonLeft(id) {
+        if (id[0] == "a" || Number(id[1]) == 1)
+            return false;
+        let num = Number(id[1]) - 1;
+        return `${String.fromCharCode(id[0].charCodeAt(0) - 1)}${num}`;
+    }
+    //will give top right ids
+    function topRight(id) {
+        if (id[0] == "h" || Number(id[1]) == 8)
+            return false;
+        
+        let num = Number(id[1]) + 1;
+        return `${String.fromCharCode(id[0].charCodeAt(0) + 1)}${num}`;
+    }
+    //will give bottom right ids
+    function bottonRight(id) {
+        if (id[0] == "h" || Number(id[1]) == 1)
+            return false;
+        let num = Number(id[1]) - 1;
+        return `${String.fromCharCode(id[0].charCodeAt(0) + 1)}${num}`;
+    }
+    
+    //console.log(keySquareMapper[id]);
+    console.log(topLeft("g7"));
+    //console.log(bottonLeft("a7"));
     return [];
 }
 
