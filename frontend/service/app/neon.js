41cd73fb9bfc37b11fe7cb7986f95bbf8e31c5a5
erivero-p <elrivper@gmail.com>
2024-12-17 11:29:11 +0100

    neon works better

--- frontend/service/app/neon.js
+++ frontend/service/app/neon.js
@@ -2,111 +2,67 @@ const color1 = "#00F56A";
 const color2 = "#00F56Acc";
 const colorT1 = "rgba(0, 245, 106, 0.4)"; // 40% opacity
 const colorT2 = "rgba(0, 245, 106, 0.3)"; // 30% opacity
-function animateNeon(element, maxShadow, interval) {
+
+function animateNeon(element, animationFeatures) {
+    const { maxShadow, interval, blinkTimes, isTextShadow, stopOnComplete } = animationFeatures;
+
     let shadowSize = 0;
-    let increment = true;
-
-    setInterval(() => {
-        element.style.textShadow = `
-            0 0 ${shadowSize}px ${colorT2}, 
-            0 0 ${shadowSize * 2}px ${colorT1}, 
-            0 0 ${shadowSize * 3}px ${colorT1}, 
-            0 0 ${shadowSize * 4}px ${colorT1}`;
-        if (increment) {
-            shadowSize++;
-            if (shadowSize >= maxShadow) increment = false;
-        } else {
-            shadowSize--;
-            if (shadowSize <= 0) increment = true;
-        }
-    }, interval);
-}
-
-function animateNeonFrame(element, maxShadow, interval, blinkTimes, restDuration) {
-    let shadowSize = maxShadow;
     let decrement = true;
     let blinkCount = 0;
-    let isResting = false;
 
     const animationInterval = setInterval(() => {
-        if (isResting) {
-            /* during rest we keep max shadow */
-            element.style.boxShadow = `
-                0 0 ${maxShadow}px ${color2}, 
-                0 0 ${maxShadow * 2}px ${color1}, 
-                0 0 ${maxShadow * 3}px ${color1}`;
-            return;
-        }
-        /* changing box shadow when is not resting */
-        element.style.boxShadow = `
-            0 0 ${shadowSize}px ${color2}, 
-            0 0 ${shadowSize * 2}px ${color1}, 
-            0 0 ${shadowSize * 3}px ${color1}`;
-        /* adjusting shadow size incrementing/decrementing */
+        setShadow(element, shadowSize, isTextShadow);
+
         if (decrement) {
             shadowSize--;
-            if (shadowSize <= 0) {
-                decrement = false;
-            }
+            if (shadowSize <= 0) decrement = false;
         } else {
             shadowSize++;
             if (shadowSize >= maxShadow) {
                 decrement = true;
                 blinkCount++;
 
-                if (blinkCount >= blinkTimes) {
-                    isResting = true;
-                    setTimeout(() => {
-                        isResting = false;
-                        blinkCount = 0;
-                    }, restDuration);
+                if (stopOnComplete && blinkCount >= blinkTimes) {
+                    clearInterval(animationInterval);
+                    setShadow(element, maxShadow, isTextShadow);
                 }
             }
         }
     }, interval);
+
+    return animationInterval;
 }
 
-function animateNeonCard(element, maxShadow, interval, blinkTimes, restDuration) {
-    let shadowSize = maxShadow;
-    let decrement = true;
-    let blinkCount = 0;
-    let isResting = false;
+function setShadow(element, shadowSize, isTextShadow) {
+    const shadowStyle = isTextShadow
+        ? `
+            0 0 ${shadowSize}px ${colorT2}, 
+            0 0 ${shadowSize * 2}px ${colorT1}, 
+            0 0 ${shadowSize * 3}px ${colorT1}, 
+            0 0 ${shadowSize * 4}px ${colorT1}`
+        : `
+            0 0 ${shadowSize}px ${color2}, 
+            0 0 ${shadowSize * 2}px ${color1}, 
+            0 0 ${shadowSize * 3}px ${color1}`;
+
+    if (isTextShadow) {
+        element.style.textShadow = shadowStyle;
+    } else {
+        element.style.boxShadow = shadowStyle;
+    }
+}
+
+function animateNeonCardHover(element, maxShadow, interval, blinkTimes) {
     let animationInterval;
 
     function startAnimation() {
-        animationInterval = setInterval(() => {
-            if (isResting) {
-                element.style.boxShadow = `
-                    0 0 ${maxShadow}px ${color2}, 
-                    0 0 ${maxShadow * 2}px ${color1}, 
-                    0 0 ${maxShadow * 3}px ${color1}`;
-                return;
-            }
-            element.style.boxShadow = `
-                0 0 ${shadowSize}px ${color2}, 
-                0 0 ${shadowSize * 2}px ${color1}, 
-                0 0 ${shadowSize * 3}px ${color1}`;
-            if (decrement) {
-                shadowSize--;
-                if (shadowSize <= 0) {
-                    decrement = false;
-                }
-            } else {
-                shadowSize++;
-                if (shadowSize >= maxShadow) {
-                    decrement = true;
-                    blinkCount++;
-
-                    if (blinkCount >= blinkTimes) {
-                        isResting = true;
-                        setTimeout(() => {
-                            isResting = false;
-                            blinkCount = 0;
-                        }, restDuration);
-                    }
-                }
-            }
-        }, interval);
+        animationInterval = animateNeon(element, {
+            maxShadow,
+            interval,
+            blinkTimes,
+            isTextShadow: false,
+            stopOnComplete: true,
+        });
     }
 
     function stopAnimation() {
@@ -118,35 +74,33 @@ function animateNeonCard(element, maxShadow, interval, blinkTimes, restDuration)
     element.addEventListener('mouseleave', stopAnimation);
 }
 
-function animateCardHover() {
-    const cards = document.querySelectorAll('.card-hover');
-    cards.forEach(card => {
-        animateNeonCard(card, 10, 35, 2, 1000);
-    });
-}
 
 function initializeNeonFrames() {
     const neonText = document.getElementById("neon-text");
     const neonFrames = document.querySelectorAll(".blinking-neon-frame");
+    const cards = document.querySelectorAll('.card-hover');
     
     if (neonText) {
-        animateNeon(neonText, 10, 100);
+        animateNeon(neonText, { maxShadow: 10, interval: 100, isTextShadow: true, stopOnComplete: false });
     }
     if (neonFrames) {
         neonFrames.forEach(neonFrame => {
-            animateNeonFrame(neonFrame, 10, 35, 2, 1000);
+            animateNeon(neonFrame, { maxShadow: 10, interval: 35, blinkTimes: 2, isTextShadow: false, stopOnComplete: false });
         });
     }
-    animateCardHover();
+    if (cards) {
+        cards.forEach(card => {
+            animateNeonCardHover(card, 10, 35, 2);
+        });
+    }
 }
 
 function initializeNeonChat() {
     const notification = document.getElementById("notification-indicator");
-    
+
     if (notification) {
-        animateNeonFrame(notification, 10, 35, 2, 1000);
+        animateNeon(notification, { maxShadow: 10, interval: 35, blinkTimes: 2, isTextShadow: false, stopOnComplete: false });
     }
 }
 
-export {initializeNeonFrames, initializeNeonChat}
-
+export { initializeNeonFrames, initializeNeonChat };
