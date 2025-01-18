42fbc62bd7566678039654c11133f2a53de2f655
Juan Ariza Ordonez <juanarizaordonez@gmail.com>
2025-01-18 12:15:44 +0100

    I think SOCIAL APIs finished

--- social/service/social/api/serializers.py
+++ social/service/social/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from social.models import User, Status, Friends
+from social.models import User, Status, InvitationRequest
 
 # PROFILE SERIALIZERS
 class ProfileSerializer(serializers.ModelSerializer):
@@ -32,24 +32,40 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 
 # FRIENDS LIST SERIALIZER
-class FriendsListSerializer(serializers.ModelSerializer): # TODO es antiguo habrá que cambairlo
-    friends = serializers.SerializerMethodField()
-    pending_friends = serializers.SerializerMethodField()
-    blocked = serializers.SerializerMethodField()
+class InvitationRequestSerializer(serializers.ModelSerializer):
+    sender_username = serializers.CharField(source='sender.username', read_only=True)
+    receiver_username = serializers.CharField(source='receiver.username', read_only=True)
 
     class Meta:
-        model = Friends
-        fields = ['user', 'friends', 'pending_friends', 'blocked']
+        model = InvitationRequest
+        fields = ['user_id', 'sender', 'receiver', 'sender_username', 'receiver_username', 'status', 'timestamp']
 
-    def get_friends(self, obj):
-        return self._get_user_details(obj.friends)
 
-    def get_pending_friends(self, obj):
-        return self._get_user_details(obj.pending_friends)
 
-    def get_blocked(self, obj):
-        return self._get_user_details(obj.blocked)
 
-    def _get_user_details(self, user_ids):
-        users = User.objects.filter(id__in=user_ids)
-        return [{'id': user.id, 'user': user.user} for user in users]
+
+
+
+
+
+# class FriendsListSerializer(serializers.ModelSerializer): # TODO es antiguo habrá que cambairlo
+#     friends = serializers.SerializerMethodField()
+#     pending_friends = serializers.SerializerMethodField()
+#     blocked = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = Friends
+#         fields = ['user', 'friends', 'pending_friends', 'blocked']
+
+#     def get_friends(self, obj):
+#         return self._get_user_details(obj.friends)
+
+#     def get_pending_friends(self, obj):
+#         return self._get_user_details(obj.pending_friends)
+
+#     def get_blocked(self, obj):
+#         return self._get_user_details(obj.blocked)
+
+#     def _get_user_details(self, user_ids):
+#         users = User.objects.filter(id__in=user_ids)
+#         return [{'id': user.id, 'user': user.user} for user in users]
--- social/service/social/api/views/friends.py
+++ social/service/social/api/views/friends.py
@@ -8,54 +8,211 @@ from social.api.serializers import FriendsListSerializer
 import jwt
 from django.http import JsonResponse
 from config.settings import JWT_SECRET
+from django.shortcuts import get_object_or_404
 
-class FriendListView(APIView):
+from social.models import User, InvitationRequest
+from social.api.serializers import InvitationRequestSerializer
+
+class PendingReceivedRequestsView(APIView):
     """
-        Content:
-            This return the all friend lists.
+    Returns the pending friend requests received by the user.
+    """
+    
 
-        Args:
-            APIView:
-                Class from Django Rest Framework used for create views for HTTP request.
-
-        Returns:
-            The friend list or a error message
-      """
     def get(self, request):
-        # TODO hacer una funcion con esto y usarlo pra la apis, asi estaría autenticado
-        auth_header = request.META.get('HTTP_AUTHORIZATION', '')
 
-        if not auth_header.startswith('Bearer '):
-            return JsonResponse({'error': 'Invalid or missing Authorization header'}, status=status.HTTP_401_UNAUTHORIZED)
-
-        # Extraer el token JWT (después de 'Bearer ')
-        jwt_token = auth_header.split('Bearer ')[1]
         try:
-            decoded_payload = jwt.decode(jwt_token, JWT_SECRET, algorithms=['HS256'])
-        except:
-            return Response({'error: invalid auth cookie'}, status=status.HTTP_401_UNAUTHORIZED)
-        # TE DEVUELVE EL ID: user_id = decoded_payload["id"]
+            auth_header = request.META.get('HTTP_AUTHORIZATION', '')
+
+            if not auth_header.startswith('Bearer '):
+                return JsonResponse({'error': 'Invalid or missing Authorization header'}, status=status.HTTP_401_UNAUTHORIZED)
+
+            jwt_token = auth_header.split('Bearer ')[1]
+            try:
+                decoded_payload = jwt.decode(jwt_token, JWT_SECRET, algorithms=['HS256'])
+            except:
+                return Response({'error: invalid auth cookie'}, status=status.HTTP_401_UNAUTHORIZED)
+
+            # Verify the user
+            user_data = User.objects.get(user_id=decoded_payload["user_id"])
+
+            pending_requests = user_data.received_requests.filter(status=InvitationRequest.Status.PENDING)
+            serializer = InvitationRequestSerializer(pending_requests, many=True)
+            return Response(serializer.data)
+        
+        except User.DoesNotExist:
+            return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+class PendingSentRequestsView(APIView):
+    """
+    Returns the pending friend requests sent by the user.
+    """
+    def get(self, request):
         try:
-            friend_data = Friends.objects.get(user_id=decoded_payload["user_id"])
-            serializer = FriendsListSerializer(friend_data)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except Friends.DoesNotExist:
-            return Response({'error': 'Friend data not found'}, status=status.HTTP_404_NOT_FOUND)
+            auth_header = request.META.get('HTTP_AUTHORIZATION', '')
 
-# class PendingFriendListView(APIView):
-#     def get(self, request, user_id):
-#         try:
-#             friend_data = Friends.objects.get(user_id=user_id)
-#             serializer = FriendsListSerializer.get_pending_friends(friend_data)
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#         except Friends.DoesNotExist:
-#             return Response({'error': 'Friend data not found'}, status=status.HTTP_404_NOT_FOUND)
+            if not auth_header.startswith('Bearer '):
+                return JsonResponse({'error': 'Invalid or missing Authorization header'}, status=status.HTTP_401_UNAUTHORIZED)
 
-# class BlockedFriendListView(APIView):
-#     def get(self, request, user_id):
-#         try:
-#             friend_data = Friends.objects.get(user_id=user_id)
-#             serializer = FriendsListSerializer.get_blocked(friend_data)
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#         except Friends.DoesNotExist:
-#             return Response({'error': 'Friend data not found'}, status=status.HTTP_404_NOT_FOUND)
+            jwt_token = auth_header.split('Bearer ')[1]
+            try:
+                decoded_payload = jwt.decode(jwt_token, JWT_SECRET, algorithms=['HS256'])
+            except:
+                return Response({'error: invalid auth cookie'}, status=status.HTTP_401_UNAUTHORIZED)
+
+            # Verify the user
+            user_data = User.objects.get(user_id=decoded_payload["user_id"])
+
+            pending_requests = user_data.sent_requests.filter(status=InvitationRequest.Status.PENDING)
+            serializer = InvitationRequestSerializer(pending_requests, many=True)
+            return Response(serializer.data)
+        
+        except User.DoesNotExist:
+            return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+class AcceptRequestView(APIView):
+    """
+    Accepts a friend request.
+    """
+    def post(self, request):
+        try:
+            auth_header = request.META.get('HTTP_AUTHORIZATION', '')
+
+            if not auth_header.startswith('Bearer '):
+                return JsonResponse({'error': 'Invalid or missing Authorization header'}, status=status.HTTP_401_UNAUTHORIZED)
+
+            jwt_token = auth_header.split('Bearer ')[1]
+            try:
+                decoded_payload = jwt.decode(jwt_token, JWT_SECRET, algorithms=['HS256'])
+            except:
+                return Response({'error: invalid auth cookie'}, status=status.HTTP_401_UNAUTHORIZED)
+
+            # Verify the user
+            current_user = User.objects.get(user_id=decoded_payload["user_id"])
+            # Other User
+            other_username = request.data.get("username")
+            if not other_username:
+                return Response({"error": "The other Username is required."}, status=status.HTTP_400_BAD_REQUEST)
+            
+            try:
+                # Get the user corresponding to the username
+                other_user = get_object_or_404(User, username=other_username)
+
+                # Find the pending request where the logged-in user is the receiver
+                invitation_request = InvitationRequest.objects.get(
+                    sender=other_user, receiver=current_user, status=InvitationRequest.Status.PENDING
+                )
+
+                # Update the status of the request
+                invitation_request.status = InvitationRequest.Status.ACCEPTED
+                invitation_request.save()
+
+                # Add friends mutually
+                current_user.friends.append(other_user.username)
+                other_user.friends.append(current_user.username)
+                current_user.save()
+                other_user.save()
+
+                return Response({"message": "Request Accepted."}, status=status.HTTP_200_OK)
+            except InvitationRequest.DoesNotExist:
+                return Response({"error": "Request NOT FOUND"}, status=status.HTTP_404_NOT_FOUND)
+            
+        except User.DoesNotExist:
+            return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+class DeclineRequestView(APIView):
+    """
+    Rechaza una solicitud de amistad.
+    """
+    def post(self, request):
+        try:
+            auth_header = request.META.get('HTTP_AUTHORIZATION', '')
+
+            if not auth_header.startswith('Bearer '):
+                return JsonResponse({'error': 'Invalid or missing Authorization header'}, status=status.HTTP_401_UNAUTHORIZED)
+
+            jwt_token = auth_header.split('Bearer ')[1]
+            try:
+                decoded_payload = jwt.decode(jwt_token, JWT_SECRET, algorithms=['HS256'])
+            except:
+                return Response({'error: invalid auth cookie'}, status=status.HTTP_401_UNAUTHORIZED)
+
+            # Verify the user
+            current_user = User.objects.get(user_id=decoded_payload["user_id"])
+            # Other User
+            other_username = request.data.get("username")
+            if not other_username:
+                return Response({"error": "The other Username is required."}, status=status.HTTP_400_BAD_REQUEST)
+            
+            try:
+                # Get the user corresponding to the username
+                other_user = get_object_or_404(User, username=other_username)
+
+                # Find the pending request where the logged-in user is the receiver
+                invitation_request = InvitationRequest.objects.get(
+                sender=other_user, receiver=current_user, status=InvitationRequest.Status.PENDING
+                )
+
+                # Update the status of the request
+                invitation_request.status = InvitationRequest.Status.DECLINED
+                invitation_request.save()
+
+                return Response({"message": "Request Declined."}, status=status.HTTP_200_OK)
+            except InvitationRequest.DoesNotExist:
+                return Response({"error": "Request NOT FOUND"}, status=status.HTTP_404_NOT_FOUND)
+            
+        except User.DoesNotExist:
+            return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+class CancelSentRequestView(APIView):
+    """
+    Cancels a friend request sent by the user.
+    """
+    def post(self, request):
+        try:
+            auth_header = request.META.get('HTTP_AUTHORIZATION', '')
+
+            if not auth_header.startswith('Bearer '):
+                return JsonResponse({'error': 'Invalid or missing Authorization header'}, status=status.HTTP_401_UNAUTHORIZED)
+
+            jwt_token = auth_header.split('Bearer ')[1]
+            try:
+                decoded_payload = jwt.decode(jwt_token, JWT_SECRET, algorithms=['HS256'])
+            except:
+                return Response({'error: invalid auth cookie'}, status=status.HTTP_401_UNAUTHORIZED)
+
+            # Verify the user
+            current_user = User.objects.get(user_id=decoded_payload["user_id"])
+            # Other User
+            other_username = request.data.get("username")
+            if not other_username:
+                return Response({"error": "The other Username is required."}, status=status.HTTP_400_BAD_REQUEST)
+            
+            try:
+                # Get the user corresponding to the username
+                other_user = get_object_or_404(User, username=other_username)
+
+                # Find the pending request where the logged-in user is the receiver
+                invitation_request = InvitationRequest.objects.get(
+                sender=current_user, receiver=other_user, status=InvitationRequest.Status.PENDING
+                )
+
+                # Delete the request
+                invitation_request.delete()
+
+                return Response({"message": "Request Deleted."}, status=status.HTTP_200_OK)
+            except InvitationRequest.DoesNotExist:
+                return Response({"error": "Request NOT FOUND"}, status=status.HTTP_404_NOT_FOUND)
+            
+        except User.DoesNotExist:
+            return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
