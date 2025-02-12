566bf7b1c96ad97ebefe749ccfe9bbc9de197bd3
erivero-p <elrivper@gmail.com>
2025-02-12 19:22:51 +0100

    still with friend requests

--- frontend/service/components/friends/requests.js
+++ frontend/service/components/friends/requests.js
@@ -1,6 +1,7 @@
 import { throwAlert } from "../../app/render.js";
 import { searchUsers, sendFriendRequest, 
-    getPendingSentRequests, getPendingReceivedRequests,
+    getPendingSentRequests, getPendingReceivedRequests, cancelFriendRequest,
+
     blockUser, isUserBlocked } from "../../app/social.js";
 import { getUsername } from "../../app/auth.js";
 
@@ -26,7 +27,7 @@ export function initSearchFriendEvents(elements) {
 
 export async function renderPendingFriendRequests(container) {
     const requests = await handleGetPendingReceivedRequests();
-    if (requests.length === 0) {
+    if (!requests ||requests.length === 0) {
         container.innerText = "You don't have any friend requests yet, but you can add one!";
     } else {
         requests.forEach(request => {
@@ -39,39 +40,35 @@ export async function renderPendingFriendRequests(container) {
 
 function createFriendRequestCard( request ) {
 //if request only have the username i'll have to get the photo
-        // `<div class="user-card d-flex flex-column flex-md-row justify-content-between align-items-center">
-        //     <div class="d-flex align-items-center mb-2 mb-md-0">
-        //     <img src="${user.avatar}" alt="${user.username}" class="friend-picture">
-        //     <p class="mb-0 ms-2">${user.username}</p>
-        //     </div>
-        //     <div>
-        //     <button class="btn ctm-btn">
-        //         <i class="fas fa-check"> accept </i>
-        //     </button>
-        //     <button class="btn ctm-btn-secondary">
-        //         <i class="fas fa-cancel"> decline </i>
-        //     </button>
-        // </div>`
+    const username = request.sender__username;
+    const card = document.createElement('li');
+    card.innerHTML = 
+        `<div class="user-card d-flex flex-column flex-md-row justify-content-between align-items-center">
+            <div class="d-flex align-items-center mb-2 mb-md-0">
+            <img src="${'avatar'}" alt="${username}" class="friend-picture">
+            <p class="mb-0 ms-2">${username}</p>
+            </div>
+            <div>
+            <button class="btn ctm-btn">
+                <i class="fas fa-check"> accept </i>
+            </button>
+            <button class="btn ctm-btn-secondary">
+                <i class="fas fa-cancel"> decline </i>
+            </button>
+        </div>`
+    return (card);
 }
 
 async function handleGetPendingReceivedRequests() {
     const response = await getPendingReceivedRequests();
     if (response.status === "success") {
+        console.log("getPendingReceivedRequests says: ", response);
         return (response.incoming);
-/*         if (response.users.length === 0) {
-            return (null);
-        } else {
-            console.log(response.data);
-            return (response.users);
-        } */
-       console.log("getPendingReceivedRequests says: ", response);
     } else {
         throwAlert(response.message);
         return (null);
     } 
 }
-
-
 
 
 function toggleSearch(on, elements) {
@@ -101,7 +98,7 @@ async function searchNewFriend(elements) {
     const username = elements.searchInput.value;
     if (!username) return;
     elements.searchInput.value = '';
-    console.log('Searching for:', username);
+//    console.log('Searching for:', username);
     const users = await handleSearchUsers(username);
     const userStatusMap = await filterSearchList(users);
     renderSearchList(userStatusMap, elements);
@@ -113,7 +110,7 @@ async function handleSearchUsers(username) {
         if (response.users.length === 0) {
             return (null);
         } else {
-            console.log(response.data);
+       //     console.log(response.data);
             return (response.users);
         }
     } else {
@@ -149,7 +146,10 @@ async function getUserStatusMap(users) {
     // Log the structure of pendingRequests
     console.log('pendingRequests structure: ', JSON.stringify(pendingRequests, null, 2));
 
-    const pendingUsernames = (pendingRequests.status === "success" && Array.isArray(pendingRequests.outgoing)) ? pendingRequests.outgoing.map(request => request.username) : [];
+    const pendingUsernames = (pendingRequests.status === "success" && Array.isArray(pendingRequests.outgoing)) ? pendingRequests.outgoing.map(request => ({
+        username: request.receiver__username,
+        invitationId: request.id
+    })) : [];
 
     // Log the pendingUsernames array
     console.log('pendingUsernames: ', pendingUsernames);
@@ -157,9 +157,11 @@ async function getUserStatusMap(users) {
     for (const user of users) {
         const response = await isUserBlocked(user.username);
         const isBlocked = response.status === "success" && response.is_blocked;
-        const isPending = pendingUsernames.includes(user.username);
-        console.log(`User: ${user.username}, isBlocked: ${isBlocked}, isPending: ${isPending}`);
-        userStatusMap.set(user.username, { user, isBlocked, isPending });
+        const pendingRequest = pendingUsernames.find(pending => pending.username === user.username);
+        const isPending = !!pendingRequest;
+        const invitationId = pendingRequest ? pendingRequest.invitationId : null;
+        console.log(`User: ${user.username}, isBlocked: ${isBlocked}, isPending: ${isPending}, invitationId: ${invitationId}`);
+        userStatusMap.set(user.username, { user, isBlocked, isPending, invitationId });
     }
     return userStatusMap;
 }
@@ -175,19 +177,19 @@ function renderSearchList(users, elements) {
     } else {
         users.forEach((value, key) => {
             const status = value.isBlocked ? 'blocked' : value.isPending ? 'pendant' : 'default'
-            const addFriendCard = createAddFriendCard(value.user, status);
+            const addFriendCard = createAddFriendCard(value.user, status, value.invitationId);
             elements.searchList.appendChild(addFriendCard);
         });
     }
 }
 
-function createAddFriendCard(user, status) {
+function createAddFriendCard(user, status, invitationId) {
     const card = document.createElement('div');
     card.className = 'user-card d-flex flex-column flex-md-row justify-content-between align-items-center';
     if (!user) {
         createEmptyUserCard(card);
     } else {
-        const btns = createCardBtns(card, user);
+        const btns = createCardBtns(card, user, invitationId);
         card.setAttribute('data-user-id', user.user_id);
         card.innerHTML = `
         <div class="d-flex align-items-center mb-2 mb-md-0">
@@ -207,33 +209,44 @@ function createEmptyUserCard(card) {
         No matches found :c
     </div>`;
 }
-function createCardBtns(card, user) {
+function createCardBtns(card, user, invitationId = null) {
     const btnsContainer = document.createElement('div');
-    btnsContainer.className = 'card-btns d-flex';
+    btnsContainer.className = 'card-btns d-flex aling-items-center';
 
     const addBtn = document.createElement('button');
     addBtn.className = 'btn ctm-btn-secondary me-2';
     addBtn.setAttribute('data-action', 'add');
-    addBtn.innerHTML = '<i class="fas fa-plus"> add </i>';
+    addBtn.innerHTML = '<i class="fas fa-plus mt-1"></i> <span class="ctm-text"> add </span>';
     addBtn.addEventListener('click', () => handleSendFriendRequest(user.username, card));
 
     const blockBtn = document.createElement('button');
     blockBtn.className = 'btn ctm-btn-danger';
     blockBtn.setAttribute('data-action', 'block');
-    blockBtn.innerHTML = '<i class="fas fa-ban"> block </i>';
+    blockBtn.innerHTML = '<i class="fas fa-ban mt-1"></i> <span class="ctm-text"> block </span>';
     blockBtn.addEventListener('click', () => handleBlockUser(user.username, card));
 
     const blockedBtn = document.createElement('button');
     blockedBtn.className = 'btn ctm-btn-danger';
     blockedBtn.setAttribute('data-action', 'blocked');
-    blockedBtn.innerHTML = '<i class="fas fa-ban"> this user is blocked </i>';
+    blockedBtn.innerHTML = '<i class="fas fa-ban mt-1"></i> <span class="ctm-text"> this user is blocked </span>';
     blockedBtn.disabled = true;
 
     const pendantBtn = document.createElement('button');
     pendantBtn.className = 'btn ctm-btn pendant-btn';
     pendantBtn.setAttribute('data-action', 'pendant');
-    pendantBtn.innerHTML = '<i class="fas fa-ban"> pendant </i>';
-    pendantBtn.addEventListener('click', () => handlePendant(user.username, card));
+    pendantBtn.innerHTML = '<i class="fas fa-ban mt-1 me-2"></i> <span class="ctm-text"> pendant </span>';
+    pendantBtn.addEventListener('click', () => handleCancelFriendRequest(pendantBtn, card, invitationId));
+    pendantBtn.dataset.invitationId = invitationId;
+    console.log('on create card ID: ', invitationId);
+    // Add event listeners for hover effect
+    pendantBtn.addEventListener('mouseenter', () => {
+        pendantBtn.dataset.originalHtml = pendantBtn.innerHTML;
+        pendantBtn.innerText = 'cancel X';
+    });
+
+    pendantBtn.addEventListener('mouseleave', () => {
+        pendantBtn.innerHTML = 'pendant';
+    });
 
     btnsContainer.appendChild(addBtn);
     btnsContainer.appendChild(blockBtn);
@@ -243,14 +256,12 @@ function createCardBtns(card, user) {
     return btnsContainer;
 }
 
-
-
-function toggleBtns(card, status) {
+function toggleBtns(card, status, invitationId = null) {
     const addBtn = card.querySelector('[data-action="add"]');
     const blockBtn = card.querySelector('[data-action="block"]');
     const blockedBtn = card.querySelector('[data-action="blocked"]');
     const pendantBtn = card.querySelector('[data-action="pendant"]');
-    console.log('togglebtns status: ', status)
+//    console.log('togglebtns status: ', status)
     if (status === 'blocked') {
         addBtn.style.display = 'none';
         blockBtn.style.display = 'none';
@@ -262,12 +273,17 @@ function toggleBtns(card, status) {
         blockBtn.style.display = 'none';
         blockedBtn.style.display = 'none';
         pendantBtn.style.display = 'flex';
+        // if (invitationId) {
+        //     pendantBtn.dataset.invitationId = invitationId; 
+        //     console.log('invitation_id : ', invitationId);
+        // }
     } else {
         addBtn.style.display = 'flex';
         blockBtn.style.display = 'flex';
         blockedBtn.style.display = 'none';
         pendantBtn.style.display = 'none';
         card.classList.remove('blocked');
+        pendantBtn.dataset.invitationId = ''; 
     }
 }
 
@@ -282,11 +298,24 @@ async function handleBlockUser(username, card) {
 
 async function handleSendFriendRequest(username, card) {
     const response = await sendFriendRequest(username);
+    console.log('handleSendFriendRequest response:', response);
     if (response.status === "success") {
+        console.log("RESPONSE",response);
+
         toggleBtns(card, 'pendant');
-        throwAlert(`Friend request sent to ${username}`)
+        throwAlert(`Friend request sent to ${username}`);
+    } else {
+        throwAlert(response.message);
+    }
+}
+async function handleCancelFriendRequest(btn, card, invitationId) {
+    const response = await cancelFriendRequest(invitationId);
+    if (response.status === "success") {
+        toggleBtns(card, 'default');
     } else {
         throwAlert(response.message);
     } 
 }
+
+
 
