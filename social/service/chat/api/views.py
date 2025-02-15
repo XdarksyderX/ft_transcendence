cea5c65b4c800d65a045b0d0d95f45259739baf6
Juan Ariza Ordonez <juanarizaordonez@gmail.com>
2025-02-15 17:55:40 +0100

    Problem with social solved

--- social/service/chat/api/views.py
+++ social/service/chat/api/views.py
@@ -1,16 +1,12 @@
-from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
-from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.http import Http404
-from core.models import User, Messages
-from .serializers import MessagesSerializer
-import jwt
-from django.http import JsonResponse
-from config.settings import JWT_SECRET
+from core.models import User
+from core.utils.event_domain import publish_event
+from rest_framework.permissions import IsAuthenticated
+
 
 
 class CreateMessageView(APIView):
