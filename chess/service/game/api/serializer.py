13693590c617babecc744c8e698b916397b74ad3
jariza-o <juanarizaordonez@gmail.com>
2025-02-22 15:18:33 +0100

    Chess Models

--- chess/service/game/api/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from service.game.models import ChessGame
-
-class ChessGameSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ChessGame
-        fields = ['game_id', 'player_one', 'player_two', 'game_type', 'status', 'winner', 'created_at', 'updated_at']
--- chess/service/game/apps.py
+++ chess/service/game/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class GameConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'game'
