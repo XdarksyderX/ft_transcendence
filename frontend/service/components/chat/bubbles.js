515f868753f5f7487bc8b0d1f9a0dd5ccc46b937
erivero-p <elrivper@gmail.com>
2025-02-23 11:34:06 +0100

    sender has more accurate invitation render now

--- frontend/service/components/chat/bubbles.js
+++ frontend/service/components/chat/bubbles.js
@@ -15,14 +15,52 @@ export function createMessageBubble(message) {
 	`;
 	return card;
 }
+/* * * * * * * * * * * * * * * * * * *  SPECIAL MESSAGE BUBBLES  * * * * * * * * * * * * * * * * * * */
+
+export function createSpecialBubble(message) {
+    const messageContent = JSON.parse(message.message);
+    const invitationType = messageContent.type;
+    const isSender = message.sender === getUsername();
+
+    let card;
+    switch (invitationType) {
+        case 'pong-match':
+            if (isSender) {
+                card = createQuickGameSent('Pong', message.receiver);
+            } else {
+                card = createQuickGameInvitation(message);
+            }
+            break;
+        default:
+            console.warn(`Unknown invitation type: ${invitationType}`);
+            card = document.createElement('div');
+            card.innerHTML = `<p>Unknown invitation type: ${invitationType}</p>`;
+    }
+
+    return card;
+}
 
 /* * * * * * * * * * * * * * * * * * *  QUICK GAME INVITATIONS  * * * * * * * * * * * * * * * * * * */
 
+function createQuickGameSent(game, friend) {
+    const card = document.createElement('div');
+    card.innerHTML = `
+        <div class="card-border special-bubble" style="max-width: 250px;">
+            <div class="game-invitation card overflow-hidden">
+                <div class="card-body position-relative p-2" style="z-index: 1;">
+                    <h6 class="mb-2 ctm-text-title">${game} invitation!</h6>
+                    <p class="card-text mb-2 small ctm-text-light">You sent a ${game} invitation to ${friend}</p>
+                </div>
+            </div>
+        </div>`;
+	return (card);
+
+}
 export function createQuickGameInvitation(message) {
 	const card = document.createElement('div');
 	const remainingTime = calculateTimeRemaining(message.sent_at); 
 	card.innerHTML = `
-		<div class="quick-game-invitation card border-0 overflow-hidden" style="max-width: 250px;">
+		<div class="quick-game-invitation special-bubble card border-0 overflow-hidden" style="max-width: 250px;">
 			<div class="progress position-absolute w-100 h-100" style="z-index: 0;" data-progress>
 				<div class="progress-bar bg-primary" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
 			</div>
@@ -88,3 +126,35 @@ function startProgressBar(remainingTime, progressBar, btns, token) {
 
 
 /* * * * * * * * * * * * * * * * * * *  TOURNAMENT INVITATIONS  * * * * * * * * * * * * * * * * * * */
+
+
+/* function createTournamentInvitation(friendName, tourName) {
+	return `
+	<div class="outer-card" style="max-width: 250px;">
+		<div class="game-invitation card overflow-hidden">
+			<div class="card-body position-relative p-2" style="z-index: 1;">
+				<h6 class="mb-2 ctm-text-title">new tournament</h6>
+				<p class="card-text mb-2 small ctm-text-light"><span id="inviter-name">${friendName}</span> invited you to the <span id="tournament-name"> ${tourName} </span>tournament!</p>
+				<div class="d-flex justify-content-between align-items-center">
+					<button id="accept-btn" class="btn btn-sm ctm-btn flex-grow-1 me-1">Accept</button>
+					<button id="decline-btn" class="btn btn-sm ctm-btn-danger flex-grow-1 ms-1">Decline</button>
+				</div>
+			</div>
+		</div>
+	</div> `;
+}
+
+function createTournamentStartedNotification(tourName) {
+	return `
+	<div class="card-border" style="max-width: 250px;">
+		<div class="game-invitation card overflow-hidden">
+			<div class="card-body position-relative p-2" style="z-index: 1;">
+			<h6 class="mb-2 ctm-text-title">tournament info</h6>
+			<p class="card-text mb-2 small ctm-text-light">the <span id="tournament-name"> whatever the name </span>tournament has just started!</p>
+			<div class="d-flex justify-content-between align-items-center">
+				<button id="accept-btn" class="btn btn-sm ctm-btn flex-grow-1 me-1">got it!</button>
+			</div>
+			</div>
+		</div>
+	</div> `;
+} */
