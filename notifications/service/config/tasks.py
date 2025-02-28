75dba3566de8529a4f40c102b7c83051a9a39c33
Miguel García <miguelgarciaroman8@gmail.com>
2025-02-28 12:08:48 +0100

    Notifications for user status and user deleted

--- notifications/service/config/tasks.py
+++ notifications/service/config/tasks.py
@@ -1,8 +1,6 @@
 from celery import shared_task
-import json
-from asgiref.sync import async_to_sync
-from channels.layers import get_channel_layer
-from core.models import IncomingEvent, User, Notification
+from core.models import IncomingEvent, User
+from core.utils.notifications import send_notification
 
 def event_already_processed(event_id):
     return IncomingEvent.objects.filter(event_id=event_id).exists()
@@ -12,27 +10,6 @@ def mark_event_as_processed(event_id, event_type):
         event_id=event_id,
         event_type=event_type
     )
-
-def send_notification(user_id, notification):
-    try:
-        user = User.objects.get(id=user_id)
-        notif = Notification.objects.create(
-            content=json.dumps(notification),
-            receiver=user
-        )
-        user.notifications.add(notif)
-
-        channel_layer = get_channel_layer()
-        room_group_name = f"user_{user_id}"
-        async_to_sync(channel_layer.group_send)(
-            room_group_name,
-            {
-                "type": "notification",
-                "data": notification
-            }
-        )
-    except User.DoesNotExist:
-        print(f"User with id {user_id} does not exist. Notification not sent.")
 
 @shared_task(name="auth.user_registered")
 def handle_user_registered(event):
@@ -54,112 +31,177 @@ def handle_user_registered(event):
     mark_event_as_processed(event_id, event["event_type"])
     return f"User {user_data['username']} {'created' if created else 'updated'} in notification service."
 
+@shared_task(name="auth.user_deleted")
+def handle_user_deleted(event):
+    event_id = event["event_id"]
+    if event_already_processed(event_id):
+        return f"Event {event_id} already processed."
+
+    try:
+        event_data = event["data"]["data"]["attributes"]
+        user = User.objects.get(id=event_data["user_id"])
+        notification = {
+            "event_type": "user_deleted",
+            "user": user.username
+        }
+        for friend in user.friends.all():
+            send_notification(friend.id, notification)
+        user.delete()
+        mark_event_as_processed(event_id, event["event_type"])
+        return f"User {user.username} deleted from notifications service."
+    except User.DoesNotExist:
+        return f"User with ID {event_data['id']} does not exist."
+
+@shared_task(name="auth.username_changed")
+def handle_username_changed(event):
+    event_id = event["event_id"]
+    if event_already_processed(event_id):
+        return f"Event {event_id} already processed."
+
+    try:
+        event_data = event["data"]["data"]["attributes"]
+        user = User.objects.get(id=event_data["user_id"])
+        user.username = event_data["username"]
+        user.save()
+        mark_event_as_processed(event_id, event["event_type"])
+        return f"Username updated to {event_data['username']} in notifications service."
+    except User.DoesNotExist:
+        return f"User with ID {event_data['id']} does not exist."
+
 @shared_task(name="social.friend_added")
 def handle_friend_added(event):
-    event_data = event["data"]["data"]["attributes"]
-    user_id = event_data["user_id"]
-    other_id = event_data["friend_id"]
-    notification = {
-        "event_type": "friend_added",
-        "user_id": user_id,
-        "other_id": other_id
-    }
-    send_notification(user_id, notification)
-    send_notification(other_id, notification)
-    return f"Notified both users that user_{user_id} and user_{other_id} are now friends"
+    try:
+        event_data = event["data"]["data"]["attributes"]
+        user_id = event_data["user_id"]
+        other_id = event_data["friend_id"]
+        user = User.objects.get(id=user_id)
+        other = User.objects.get(id=other_id)
+        notification = {
+            "event_type": "friend_added",
+            "user": user.username,
+            "other": other.username
+        }
+        send_notification(user_id, notification)
+        send_notification(other_id, notification)
+        user.friends.add(other)
+        other.friends.add(user)
+        return f"Notified both users that {user.username} and {other.username} are now friends"
+    except User.DoesNotExist:
+        return "Could not add friend - one or both users do not exist"
+    except Exception as e:
+        return f"Error adding friend: {str(e)}"
 
 @shared_task(name="social.friend_removed")
 def handle_friend_removed(event):
-    event_data = event["data"]["data"]["attributes"]
-    user_id = event_data["user_id"]
-    other_id = event_data["friend_id"]
-    notification = {
-        "event_type": "friend_removed",
-        "user_id": user_id,
-        "other_id": other_id
-    }
-    send_notification(user_id, notification)
-    send_notification(other_id, notification)
-    return f"Notified both users that user_{user_id} and user_{other_id} are no longer friends"
+    try:
+        event_data = event["data"]["data"]["attributes"]
+        user_id = event_data["user_id"]
+        other_id = event_data["friend_id"]
+        user = User.objects.get(id=user_id)
+        other = User.objects.get(id=other_id)
+        notification = {
+            "event_type": "friend_removed",
+            "user": user.username,
+            "other": other.username
+        }
+        send_notification(user_id, notification)
+        send_notification(other_id, notification)
+        user.friends.remove(other)
+        other.friends.remove(user)
+        return f"Notified both users that {user.username} and {other.username} are no longer friends"
+    except User.DoesNotExist:
+        return "Could not remove friend - one or both users do not exist"
+    except Exception as e:
+        return f"Error removing friend: {str(e)}"
 
 @shared_task(name="social.request_declined")
 def handle_request_declined(event):
     event_data = event["data"]["data"]["attributes"]
     user_id = event_data["user_id"]
     other_id = event_data["friend_id"]
+    user = User.objects.get(id=user_id)
+    other = User.objects.get(id=other_id)
     notification = {
         "event_type": "request_declined",
-        "user_id": user_id,
-        "other_id": other_id
+        "user": user.username,
+        "other": other.username
     }
     send_notification(user_id, notification)
     send_notification(other_id, notification)
-    return f"Notified both users that user_{user_id} declined the friend request from user_{other_id}"
+    return f"Notified both users that {user.username} declined the friend request from {other.username}"
 
 @shared_task(name="social.request_cancelled")
 def handle_request_cancelled(event):
     event_data = event["data"]["data"]["attributes"]
     user_id = event_data["user_id"]
     other_id = event_data["friend_id"]
+    user = User.objects.get(id=user_id)
+    other = User.objects.get(id=other_id)
     notification = {
         "event_type": "request_cancelled",
-        "user_id": user_id,
-        "other_id": other_id
+        "user": user.username,
+        "other": other.username
     }
     send_notification(user_id, notification)
     send_notification(other_id, notification)
-    return f"Notified both users that user_{user_id} cancelled the friend request to user_{other_id}"
+    return f"Notified both users that {user.username} cancelled the friend request to {other.username}"
 
 @shared_task(name="social.request_sent")
 def handle_request_sent(event):
     event_data = event["data"]["data"]["attributes"]
     user_id = event_data["user_id"]
     other_id = event_data["friend_id"]
+    user = User.objects.get(id=user_id)
+    other = User.objects.get(id=other_id)
     notification = {
         "event_type": "request_sent",
-        "user_id": user_id,
-        "other_id": other_id
+        "user": user.username,
+        "other": other.username
     }
     send_notification(user_id, notification)
     send_notification(other_id, notification)
-    return f"Notified both users that user_{user_id} sent a friend request to user_{other_id}"
+    return f"Notified both users that {user.username} sent a friend request to {other.username}"
 
 @shared_task(name="social.avatar_changed")
 def handle_avatar_changed(event):
     event_data = event["data"]["data"]["attributes"]
     user_id = event_data["user_id"]
+    user = User.objects.get(id=user_id)
     notification = {
         "event_type": "avatar_changed",
-        "user_id": user_id
+        "user": user.username
     }
     send_notification(user_id, notification)
-    return f"Notified user_{user_id} that their avatar has been changed"
+    return f"Notified {user.username} that their avatar has been changed"
 
 @shared_task(name="pong.match_invitation")
-def handle_friend_removed(event):
+def handle_pong_match_invitation(event):
     event_data = event["data"]["data"]["attributes"]
     sender_id = event_data["sender_id"]
     receiver_id = event_data["receiver_id"]
+    sender = User.objects.get(id=sender_id)
+    receiver = User.objects.get(id=receiver_id)
     notification = {
-        "event_type": "match_invitation",
-        "sender_id": sender_id,
-        "receiver_id": receiver_id
+        "event_type": "pong.match_invitation",
+        "user": sender.username,
+        "other": receiver.username
     }
     send_notification(receiver_id, notification)
     send_notification(sender_id, notification)
-    return f"Notified both users that user_{sender_id} invited user_{receiver_id} to a match"
-
+    return f"Notified both users that {sender.username} invited {receiver.username} to a match"
 
 @shared_task(name="pong.tournament_invitation")
-def handle_friend_removed(event):
+def handle_pong_tournament_invitation(event):
     event_data = event["data"]["data"]["attributes"]
     sender_id = event_data["sender_id"]
     receiver_id = event_data["receiver_id"]
+    sender = User.objects.get(id=sender_id)
+    receiver = User.objects.get(id=receiver_id)
     notification = {
-        "event_type": "tournament_invitation",
-        "sender_id": sender_id,
-        "receiver_id": receiver_id
+        "event_type": "pong.tournament_invitation",
+        "user": sender.username,
+        "other": receiver.username
     }
     send_notification(receiver_id, notification)
     send_notification(sender_id, notification)
-    return f"Notified both users that user_{sender_id} invited user_{receiver_id} to a tournament"
+    return f"Notified both users that {sender.username} invited {receiver.username} to a tournament"
--- notifications/service/core/models.py
+++ notifications/service/core/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     username = models.CharField(max_length=20, unique=True, verbose_name='Username')
+    friends = models.ManyToManyField(
+        'User',
+        blank=True,
+        related_name='friends'
+    )
     notifications = models.ManyToManyField(
         'Notification',
         blank=True,
