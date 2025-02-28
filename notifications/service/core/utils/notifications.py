e434436c584b41e4d43924bd6f8fe57a5eb3a1df
Miguel García <miguelgarciaroman8@gmail.com>
2025-02-28 12:08:48 +0100

    Notifications for user status and user deleted

---
+++ notifications/service/core/utils/notifications.py
@@ -0,0 +1,26 @@
+
+import json
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+from core.models import IncomingEvent, User, Notification
+
+def send_notification(user_id, notification):
+    try:
+        user = User.objects.get(id=user_id)
+        notif = Notification.objects.create(
+            content=json.dumps(notification),
+            receiver=user
+        )
+        user.notifications.add(notif)
+
+        channel_layer = get_channel_layer()
+        room_group_name = f"user_{user_id}"
+        async_to_sync(channel_layer.group_send)(
+            room_group_name,
+            {
+                "type": "notification",
+                "data": notification
+            }
+        )
+    except User.DoesNotExist:
+        print(f"User with id {user_id} does not exist. Notification not sent.")
