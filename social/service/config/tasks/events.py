fda2c13b6d62a6932af69d25dacbfff17ec42743
Miguel García <miguelgarciaroman8@gmail.com>
2025-03-04 20:23:06 +0100

    Social error fixed

--- social/service/config/tasks/events.py
+++ social/service/config/tasks/events.py
@@ -1,33 +1,37 @@
 from celery import shared_task
 from django.contrib.auth import get_user_model
 from .utils import event_already_processed, mark_event_as_processed
+from django.db import transaction
 
 User = get_user_model()
 
 @shared_task(name="events.user_connected")
 def handle_user_connected(event):
-    event_id = event["event_id"]
-    if event_already_processed(event_id):
-        return f"Event {event_id} already processed."
+	event_id = event["event_id"]
+	if event_already_processed(event_id):
+		return f"Event {event_id} already processed."
 
-    event_data = event["data"]["attributes"]
-    user = User.objects.get(id=event_data["user_id"])
-    user.is_active = True
-    user.save()
-
-    mark_event_as_processed(event_id, event["event_type"])
-    return f"User {user.username} connected to social service."
+	event_data = event["data"]["attributes"]
+	user = User.objects.get(id=event_data["user_id"])
+	with transaction.atomic():
+		user = User.objects.select_for_update().get(id=event_data["user_id"])
+		user.is_active = True
+		user.save()
+	mark_event_as_processed(event_id, event["event_type"])
+	return f"User {user.username} connected to social service."
 
 @shared_task(name="events.user_disconnected")
 def handle_user_disconnected(event):
-    event_id = event["event_id"]
-    if event_already_processed(event_id):
-        return f"Event {event_id} already processed."
+	event_id = event["event_id"]
+	if event_already_processed(event_id):
+		return f"Event {event_id} already processed."
 
-    event_data = event["data"]["attributes"]
-    user = User.objects.get(id=event_data["user_id"])
-    user.is_active = False
-    user.save()
+	event_data = event["data"]["attributes"]
+	user = User.objects.get(id=event_data["user_id"])
+	with transaction.atomic():
+		user = User.objects.select_for_update().get(id=event_data["user_id"])
+		user.is_active = True
+		user.save()
 
-    mark_event_as_processed(event_id, event["event_type"])
-    return f"User {user.username} disconnected from social service."
+	mark_event_as_processed(event_id, event["event_type"])
+	return f"User {user.username} disconnected from social service."
