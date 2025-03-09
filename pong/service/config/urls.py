82893e79c87cc632249d5fc21d4fbcc6b389509d
Miguel García <miguelgarciaroman8@gmail.com>
2025-03-09 12:48:25 +0100

    Add tournaments urls

--- pong/service/config/urls.py
+++ pong/service/config/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('matches.urls')),
 	path('', include('stats.urls')),
+	path('', include('tournaments.urls')),
 ]
 
 handler404 = 'core.exceptions.global_handler.page_not_found'
