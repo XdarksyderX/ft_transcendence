8a94ed888f9e02c81ae9598e558c52c9c4a5500a
Miguel García <miguelgarciaroman8@gmail.com>
2025-03-09 13:06:00 +0100

    maybe it works now

--- pong/service/tournaments/tournament.py
+++ pong/service/tournaments/tournament.py
@@ -1,4 +1,4 @@
-from core.models import PongGame, TournamentGame
+from core.models import PongGame, TournamentMatch
 
 def get_tournament_bracket(tournament):
     """
@@ -54,44 +54,47 @@ def create_next_round_matches(tournament, current_round):
     Creates as many next round matches as possible using the winners of the current round.
     If all matches in the current round have been played, it creates the complete next round.
     The new matches are created with 'available' set to False.
-    
-    Returns a list of new TournamentGame instances.
+    Returns a list of new TournamentMatch instances.
     """
-    # Get current round TournamentGames ordered by bracket_position.
-    current_games = tournament.tournament_games.filter(round_number=current_round).order_by('bracket_position')
+    # Get current round matches ordered by their position in the tournament
+    current_games = tournament.games.filter(
+        tournament_match__round_number=current_round
+    ).order_by('tournament_match__id')
     
-    # Collect winners from finished matches.
+    # Collect winners from finished matches
     winners = []
-    for tg in current_games:
-        if tg.match.status == 'finished' and tg.match.winner:
-            winners.append((tg.bracket_position, tg.match.winner))
+    for game in current_games:
+        if game.status == 'finished' and game.winner:
+            # Store bracket position based on game ID or another ordering mechanism
+            position = game.tournament_match.id
+            winners.append((position, game.winner))
     
-    # Sort winners by their original bracket_position.
+    # Sort winners by their original position
     winners.sort(key=lambda x: x[0])
     winners_list = [w[1] for w in winners]
     
-    # If not all current round matches are finished, only use complete pairs.
+    # If not all current round matches are finished, only use complete pairs
     if not are_all_matches_finished(tournament, current_round):
         if len(winners_list) % 2 != 0:
-            winners_list = winners_list[:-1]
+            winners_list = winners_list[:-1]  # Remove last winner if odd number
     
-    # If there are fewer than 2 winners, no match can be created.
+    # If there are fewer than 2 winners, no match can be created
     if len(winners_list) < 2:
         return []
     
     next_round = current_round + 1
-    new_tg_list = []
+    new_match_list = []
     
-    # Pair winners in order and create new matches.
+    # Pair winners in order and create new matches
     for i in range(0, len(winners_list), 2):
         if i + 1 < len(winners_list):
             p1 = winners_list[i]
             p2 = winners_list[i + 1]
         else:
-            # In case of an odd number (only possible if all matches finished), assign a bye.
+            # In case of an odd number (only possible if all matches finished), assign a bye
             p1 = winners_list[i]
-            p2 = winners_list[i]  # Duplicate to represent the bye.
-        
+            p2 = p1  # Duplicate to represent the bye
+            
         new_game = PongGame.objects.create(
             player1=p1,
             player2=p2,
@@ -104,50 +107,56 @@ def create_next_round_matches(tournament, current_round):
             speed_up_multiple=1.02,
             max_speed=20,
             points_to_win=3,
-            available=False,  # Not playable yet.
-            status='pending'
+            status='pending',
+            is_tournament=True,
+            tournament=tournament
         )
-        # Determine new bracket position (for example, based on pairing order).
-        new_bracket_position = (i // 2) + 1
-        new_tg = TournamentGame.objects.create(
+        
+        # Create TournamentMatch
+        new_match = TournamentMatch.objects.create(
             tournament=tournament,
-            match=new_game,
             round_number=next_round,
-            bracket_position=new_bracket_position
+            pong_game=new_game
         )
-        new_tg_list.append(new_tg)
+        
+        new_match_list.append(new_match)
     
-    return new_tg_list
+    return new_match_list
 
 
 def process_tournament_round_bracket(tournament, finished_game):
     """
     Processes the tournament bracket for a finished game.
-    
-    1. Verifies that the finished_game belongs to the tournament through TournamentGame.
-    2. Uses current round information to create as many next round matches as possible (via create_next_round_matches).
+    1. Verifies that the finished_game belongs to the tournament.
+    2. Uses current round information to create as many next round matches as possible.
     3. If all matches in the current round have been played, marks the next round matches as available.
-    
-    Returns a list of next round TournamentGame instances, or None if the finished_game is not part of the tournament.
+    Returns a list of next round TournamentMatch instances, or None if the finished_game is not part of the tournament.
     """
-    from django.core.exceptions import ObjectDoesNotExist
-
+    
     try:
-        current_tg = TournamentGame.objects.get(tournament=tournament, match=finished_game)
-    except TournamentGame.DoesNotExist:
-        # The finished_game is not part of the tournament.
+        # Check if this game is part of the tournament
+        if not hasattr(finished_game, 'tournament_match'):
+            return None
+            
+        tournament_match = finished_game.tournament_match
+        
+        # Verify this match belongs to the specified tournament
+        if tournament_match.tournament != tournament:
+            return None
+            
+        current_round = tournament_match.round_number
+        
+        # Create next round matches based on the current information
+        next_round_matches = create_next_round_matches(tournament, current_round)
+        
+        # If all current round matches are finished, make next round matches available
+        if are_all_matches_finished(tournament, current_round):
+            for match in next_round_matches:
+                match.pong_game.status = 'pending'
+                match.pong_game.save()
+        
+        return next_round_matches
+        
+    except:
+        # The finished_game is not part of the tournament
         return None
-
-    current_round = current_tg.round_number
-
-    # Create next round matches based on the current information.
-    next_round_games = create_next_round_matches(tournament, current_round)
-
-    # If all current round matches are finished, mark the next round matches as available.
-    if are_all_matches_finished(tournament, current_round):
-        for tg in next_round_games:
-            if not tg.match.available:
-                tg.match.available = True
-                tg.match.save()
-
-    return next_round_games
