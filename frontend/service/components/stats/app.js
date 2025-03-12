68f851a2d78d676865210e89aec781e301dd08ab
Elisa Rivero Perez <erivero-@c3r10s3.42malaga.com>
2025-03-12 15:19:01 +0100

    connecting /stats w api

--- frontend/service/components/stats/app.js
+++ frontend/service/components/stats/app.js
@@ -1,4 +1,6 @@
 import { renderMatchHistory } from "./history.js";
+import { getQuickMatchStats, getTournamentStats } from "../../app/pong.js";
+
 
 export function initializeStatsEvents() {
 	renderPongStats();
@@ -28,12 +30,16 @@ const chessStats = {
 	streak: 1,
 };
 
-function renderPongStats() {
+async function renderPongStats() {
 	const pongQuick = document.getElementById('pong-quick-stats');
-	renderQuickGameStats(pongQuick, pongStats.quickGame);
+	const quickGameStats = await handleGetQuickMatchStats();
+	if (!quickGameStats) return ;
+	renderQuickGameStats(pongQuick, quickGameStats);
 
 	const pongTournament = document.getElementById('pong-tournament-stats');
-	renderTournamentStats(pongTournament, pongStats.tournaments);
+	const tourStats = await handleGetTournamentStats();
+	if (tourStats) return ;
+	renderTournamentStats(pongTournament, tourStats);
 
 }
 
@@ -43,15 +49,36 @@ function renderChessStats() {
 }
 
 
+async function handleGetQuickMatchStats() {
+	const response = await getQuickMatchStats();
+	if (response.status === "success") {
+		return response.stats;
+	} else {
+		console.error("Error while fetching quick match stats:", response.message);
+		return null;
+	}
+}
+
+async function handleGetTournamentStats() {
+	const response = await getTournamentStats();
+	if (response.status === "success") {
+		return response.stats;
+	} else {
+		console.error("Error while fetching quick match stats:", response.message);
+		return null;
+	}
+}
+
+
 
 function renderQuickGameStats(container, stats) {
 	let winnRate = (stats.wins / stats.played * 100).toFixed(2);
   	container.innerHTML = `
 	<p><span class="fw-bold">Played:</span> ${stats.played}</p>
-	<p><span class="fw-bold">Wins:</span> ${stats.wins}</p>
-	<p><span class="fw-bold">Losses:</span> ${stats.losses}</p>
+	<p><span class="fw-bold">Wins:</span> ${stats.won}</p>
+	<p><span class="fw-bold">Losses:</span> ${stats.lost}</p>
 	<p><span class="fw-bold">Win rate:</span> ${winnRate}%</p>
-	<p><span class="fw-bold">Longest winning streak: ${stats.streak}</p>
+	<p><span class="fw-bold">Highest score: ${stats.highest_score}</p>
   `;
 }
 
