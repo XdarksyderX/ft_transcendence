c19e9aafb19ce0685fbcdc1eb614a51ca041a8f8
erivero-p <elrivper@gmail.com>
2025-03-16 12:44:41 +0100

    real chess stats included on front

--- frontend/service/components/stats/app.js
+++ frontend/service/components/stats/app.js
@@ -1,104 +1,72 @@
 import { renderMatchHistory } from "./history.js";
 import { getQuickMatchStats, getTournamentStats } from "../../app/pong.js";
-
+import { getChessStats } from "../../app/chess.js";
 
 export function initializeStatsEvents() {
-	renderPongStats();
-	renderChessStats();
-	renderMatchHistory();
+    renderPongStats();
+    renderChessStats();
+    renderMatchHistory();
 }
-
-
-const pongStats = {
-	quickGame: {
-		played: 100,
-		wins: 67,
-		losses: 33,
-		streak: 10,
-	},
-	tournaments: {
-		played: 20,
-		first: 2,
-		second: 3,
-	}
-};
-
-const chessStats = {
-	played: 30,
-	wins: 2,
-	losses: 28,
-	streak: 1,
-};
 
 async function renderPongStats() {
-	const pongQuick = document.getElementById('pong-quick-stats');
-	const quickGameStats = await handleGetQuickMatchStats();
-	if (!quickGameStats) return ;
-	renderQuickGameStats(pongQuick, quickGameStats);
+    const pongQuick = document.getElementById('pong-quick-stats');
+    const quickGameStats = await handleGetStats(getQuickMatchStats);
+    if (!quickGameStats) return ;
+    renderQuickGameStats(pongQuick, quickGameStats);
 
-	const pongTournament = document.getElementById('pong-tournament-stats');
-	const tourStats = await handleGetTournamentStats();
-	if (!tourStats) return ;
-	renderTournamentStats(pongTournament, tourStats);
-
+    const pongTournament = document.getElementById('pong-tournament-stats');
+    const tourStats = await handleGetStats(getTournamentStats);
+    if (!tourStats) return ;
+    renderTournamentStats(pongTournament, tourStats);
 }
 
-function renderChessStats() {
-	const chessQuick = document.getElementById('chess-quick-stats');
-	renderQuickGameStats(chessQuick, chessStats);
+async function renderChessStats() {
+    const chessQuick = document.getElementById('chess-quick-stats');
+    const stats = await handleGetStats(getChessStats);
+    if (!stats) return ;
+    renderChessGameStats(chessQuick, stats);
 }
 
-
-async function handleGetQuickMatchStats() {
-	const response = await getQuickMatchStats();
-	if (response.status === "success") {
-		return response.stats;
-	} else {
-		console.error("Error while fetching quick match stats:", response.message);
-		return null;
-	}
-}
-
-async function handleGetTournamentStats() {
-	const response = await getTournamentStats();
-	if (response.status === "success") {
-		return response.stats;
-	} else {
-		console.error("Error while fetching quick match stats:", response.message);
-		return null;
-	}
-}
-
-
-/* {
-    "status": "success",
-    "message": "Quick match statistics retrieved successfully",
-    "stats": {
-        "played": 1,
-        "won": 0,
-        "lost": 1,
-        "streak": 0,
-        "highest_score": 0
+async function handleGetStats(getStatsFunction) {
+    const response = await getStatsFunction();
+    if (response.status === "success") {
+        return response.stats || response.data;
+    } else {
+        console.error("Error while fetching stats", response.message);
+        return null;
     }
 }
- */
 
 function renderQuickGameStats(container, stats) {
-	let winnRate = (stats.won / stats.played * 100).toFixed(2);
-  	container.innerHTML = `
-	<p><span class="fw-bold">Played:</span> ${stats.played}</p>
-	<p><span class="fw-bold">Wins:</span> ${stats.won}</p>
-	<p><span class="fw-bold">Losses:</span> ${stats.lost}</p>
-	<p><span class="fw-bold">Win rate:</span> ${winnRate}%</p>
-	<p><span class="fw-bold">Streak:</span> ${stats.streak}</p>
-	<p><span class="fw-bold">Highest score: ${stats.highest_score}</p>
+    let winnRate = (stats.won / stats.played * 100).toFixed(2);
+      container.innerHTML = `
+    <p><span class="fw-bold">Played:</span> ${stats.played}</p>
+    <p><span class="fw-bold">Wins:</span> ${stats.won}</p>
+    <p><span class="fw-bold">Losses:</span> ${stats.lost}</p>
+    <p><span class="fw-bold">Win rate:</span> ${winnRate}%</p>
+    <p><span class="fw-bold">Streak:</span> ${stats.streak}</p>
+    <p><span class="fw-bold">Highest score: ${stats.highest_score}</p>
   `;
 }
 
 function renderTournamentStats(container, stats) {
   container.innerHTML = `
-	<p><span class="fw-bold mt-2">Tournaments Played:</span> ${stats.played}</p>
-	<p><span class="fw-bold">First place:</span> ${stats.first}</p>
-	<p><span class="fw-bold">Second place:</span> ${stats.second}</p>
+    <p><span class="fw-bold mt-2">Tournaments Played:</span> ${stats.played}</p>
+    <p><span class="fw-bold">First place:</span> ${stats.first}</p>
+    <p><span class="fw-bold">Second place:</span> ${stats.second}</p>
   `;
+}
+
+function renderChessGameStats(container, stats) {
+	console.log("hola caracola: ", stats);
+    container.innerHTML = `
+    <p><span class="fw-bold">Games Played:</span> ${stats.games_played}</p>
+    <p><span class="fw-bold">Games Won:</span> ${stats.games_won}</p>
+    <p><span class="fw-bold">Games Lost:</span> ${stats.games_lost}</p>
+    <p><span class="fw-bold">Draws:</span> ${stats.draws}</p>
+    <p><span class="fw-bold">Highest Rating:</span> ${stats.highest_rating}</p>
+	`;
+    // <p><span class="fw-bold">Username:</span> ${stats.username}</p>
+    // <p><span class="fw-bold">Created At:</span> ${new Date(stats.created_at).toLocaleString()}</p>
+    // <p><span class="fw-bold">Updated At:</span> ${new Date(stats.updated_at).toLocaleString()}</p>
 }
