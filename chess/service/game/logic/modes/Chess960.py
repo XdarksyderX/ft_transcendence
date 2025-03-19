c870b59e00cde5220884dcacec9aa5b759a780b6
erivero-p <elrivper@gmail.com>
2025-03-19 16:27:49 +0100

    fixes on horde and 960 chess

--- chess/service/game/logic/modes/Chess960.py
+++ chess/service/game/logic/modes/Chess960.py
@@ -1,29 +1,40 @@
 import random
+import logging
 from .ClassicChess import ClassicChess
-from .ChessGameMode import ChessGameMode
 from ..pieces import Rook, Knight, Bishop, Queen, King, Pawn
 from ..utils import is_checkmate, is_stalemate, is_insufficient_material
 
+logger = logging.getLogger('chess_game')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 class Chess960(ClassicChess):
     def initialize_board(self):
+        logger.debug("Initializing Chess960 board")
         board = {}
         white_back_rank = self._generate_back_rank()
         black_back_rank = white_back_rank[::-1]
+
+        # logger.debug(f"White back rank: {white_back_rank}")
+        # logger.debug(f"Black back rank: {black_back_rank}")
 
         # Place white pieces
         for idx, piece in enumerate(white_back_rank):
             position = chr(ord('a') + idx) + '1'
             board[position] = piece('white', position, str(idx + 1))
+            # logger.debug(f"Placed {piece.__name__} at {position} for white")
         
         # Place black pieces
         for idx, piece in enumerate(black_back_rank):
             position = chr(ord('a') + idx) + '8'
             board[position] = piece('black', position, str(idx + 1))
+            # logger.debug(f"Placed {piece.__name__} at {position} for black")
         
         # Place pawns
         for file in "abcdefgh":
             board[f"{file}2"] = Pawn("white", f"{file}2", str(ord(file) - ord('a') + 1))
             board[f"{file}7"] = Pawn("black", f"{file}7", str(ord(file) - ord('a') + 1))
+            logger.debug(f"Placed Pawn at {file}2 for white")
+            logger.debug(f"Placed Pawn at {file}7 for black")
         
         return board
 
--- chess/service/game/logic/modes/HordeChess.py
+++ chess/service/game/logic/modes/HordeChess.py
@@ -1,10 +1,7 @@
-from .ChessGameMode import ChessGameMode
 from .ClassicChess import ClassicChess
 from ..pieces import Rook, Knight, Bishop, Queen, King, Pawn
 from ..utils import is_in_check, is_checkmate, is_stalemate, is_insufficient_material
 
-# I guess in horde mode, the only differences are in initialization: so all black pieces are pawns
-# and in game over cause white wins if all black pieces are catched
 class HordeChess(ClassicChess):
     def initialize_board(self):
         board = {}
@@ -20,9 +17,10 @@ class HordeChess(ClassicChess):
         for file_idx, file in enumerate("abcdefgh"):
             board[f"{file}2"] = Pawn("white", f"{file}2", str(file_idx + 1))
         
-        for rank in range(1, 9):
-            for file in "abcdefgh":
-                board[f"{file}{rank}"] = Pawn("black", f"{file}{rank}", str(rank))
+        # Black pieces (all pawns on rows 1 and 2)
+        for file in "abcdefgh":
+            board[f"{file}7"] = Pawn("black", f"{file}7", "7")
+            board[f"{file}8"] = Pawn("black", f"{file}8", "8")
         
         return board
 
@@ -36,4 +34,47 @@ class HordeChess(ClassicChess):
         if is_insufficient_material(board):
             return "insufficient_material", None
         return None, None
-    
+
+    # def validate_move(self, board, from_pos, to_pos, player_color, promotion_choice=None):
+    #     if from_pos not in board or board[from_pos] is None:
+    #         return False, "No piece at the starting position", board, {}
+    #     piece = board[from_pos]
+    #     if piece.color != player_color:
+    #         return False, "You cannot move your opponent's pieces", board, {}
+
+    #     possible_moves = piece.get_possible_moves(board)
+        
+    #     # Special rule for black pawns in Horde Chess
+    #     if piece.piece_type == "pawn" and piece.color == "black":
+    #         from_rank = int(from_pos[1])
+    #         to_rank = int(to_pos[1])
+    #         if from_rank == 7 and to_rank == 5 and from_pos[0] == to_pos[0] and board[to_pos] is None:
+    #             possible_moves.append(to_pos)
+    #         if from_rank == 6 and to_rank == 4 and from_pos[0] == to_pos[0] and board[to_pos] is None:
+    #             possible_moves.append(to_pos)
+
+    #     if to_pos not in possible_moves:
+    #         return False, "Invalid move for this piece", board, {}
+
+    #     new_board = board.copy()
+    #     captured_piece = new_board[to_pos]
+    #     new_board[to_pos] = piece
+    #     new_board[from_pos] = None
+    #     piece.position = to_pos
+
+    #     if is_in_check(new_board, player_color):
+    #         return False, "You cannot make a move that leaves your king in check", board, {}
+
+    #     info = {
+    #         "captured": captured_piece.piece_id if captured_piece else None,
+    #     }
+
+    #     opponent_color = "black" if player_color == "white" else "white"
+    #     game_over_status, winner = self.check_game_over(new_board, opponent_color)
+    #     if game_over_status:
+    #         info["game_over"] = {
+    #             "status": game_over_status,
+    #             "winner": winner
+    #         }
+
+    #     return True, "Valid move", new_board, info
