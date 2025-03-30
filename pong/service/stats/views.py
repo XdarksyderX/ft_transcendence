f7188ef3fa15a49af1cc78232813eab0fb425bba
Miguel García <miguelgarciaroman8@gmail.com>
2025-03-30 19:12:44 +0200

    Fix

--- pong/service/stats/views.py
+++ pong/service/stats/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from core.models import PongStatistics
+from django.contrib.auth import get_user_model
 
 class QuickMatchStatisticsView(APIView):
 	permission_classes = [IsAuthenticated]
@@ -42,27 +43,32 @@ class TournamentStatisticsView(APIView):
 
 class PongStatisticsView(APIView):
 	permission_classes = [IsAuthenticated]
-	def get(self, request):
-		user = request.user
+	def get(self, request, username):
+		User = get_user_model()
+		
+		try:
+			user = User.objects.get(username=username)
+		except User.DoesNotExist:
+			return Response({
+				"status": "error",
+				"message": "User not found"
+			}, status=404)
+		
 		pong_statistics = PongStatistics.objects.filter(user=user).first()
 		
 		quick_data = {
 			"played": pong_statistics.quick_games_played,
 			"won": pong_statistics.quick_games_won,
 			"lost": pong_statistics.quick_games_lost,
-			"streak": pong_statistics.current_streak,
-			"highest_score": pong_statistics.highest_score
 		}
 		
 		tournament_data = {
-			"played": pong_statistics.tournaments_played,
 			"first": pong_statistics.tournaments_first,
-			"second": pong_statistics.tournaments_second,
 		}
 		
 		return Response({
 			"status": "success",
-			"message": "Combined statistics retrieved successfully",
+			"message": "Statistics retrieved successfully",
 			"stats": {
 				"quick": quick_data,
 				"tournament": tournament_data
